05bd52ff0bc76805d86ffcb23651b24607a85257
bbek3201 <bbek3201@gmail.com>
2026-03-31 12:33:00 +0800

    Building...

--- src/app/page.js
+++ src/app/page.js
@@ -1,65 +1,46 @@
 import Image from "next/image";
-
+import Input from "../components/Input";
+import { Footer } from "@/components/Footer";
 export default function Home() {
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.js file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
+    <div className="w-full h-screen bg-stone-100 flex justify-center items-center">
+      <div className="w-[480px] h-[655px] bg-white">
+        <div className="w-[416px] p-8 flex flex-col gap-2">
+          <Image
+            src="/source_Main.png"
+            alt="Main image"
+            height={60}
+            width={60}
+          />
+          <h1 className="font-bold text-black text-[26px]">Join Us! 😎</h1>
+          <p className="text-gray-400 text[16px]">
+            Please provide all current information accurately.
           </p>
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
+        <div className="flex flex-col pl-8 gap-2">
+          <Input
+            label={"First name"}
+            placeholder={"Placeholder"}
+            error={"First name cannot special characters of numbers."}
+          />
+          <Input
+            label={"Last name"}
+            placeholder={"Placeholder"}
+            error={"Last name cannot special characters of numbers."}
+          />
+          <Input
+            label={"Full name"}
+            placeholder={"Placeholder"}
+            error={"This username is already taken. Please choose another one."}
+          />
         </div>
-      </main>
+        <div
+          className="flex
+        justify-center pt-40"
+        >
+          <Footer />
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const Footer = () => {
+  return (
+    <div className="text-white">
+      <button className="border border-black w-[416px] bg-black rounded-md h-11 cursor-pointer">
+        Continue 1/3
+      </button>
+    </div>
+  );
+};
